2c4ed808807d5a1beefb9611739d671a10e95530
mahin12 <mahinarafatmeem@gmail.com>
2023-01-06 02:33:18 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,78 @@
+import logo from './logo.svg';
+import './App.css';
+import "./styles.css"
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
+import { type } from '@testing-library/user-event/dist/type';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, { type, payload }){
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === '0' && state.currentOperand === "0") return state
+      if (payload.digit === '0' && state.currentOperand.includes(".")) return state
+
+      return {
+      ...state,
+      currentOperand: '${currentOperand || ""}${payload.digit}',
+    }
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        }
+      }
+
+    case ACTIONS.CLEAR: 
+      return {}
+   }
+}
+
+function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+  
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className='previous-operand'>{previousOperand} {operation}</div>
+        <div className='current-operand'>{currentOperand}</div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button className='span-two' onClick={() => dispatch({ type: ACTIONS.CLEAR })}>=</button>      
+    </div>
+  );
+}
+
+export default App;
